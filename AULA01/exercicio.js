42fb95c627d5b8ee019c56c007ba77ed85cb00cc
Vinicius Henrique <viniciushr1402@gmail.com>
2026-09-17 16:56:01 -0300

    SOMATIVA CORREÇÃO

---
+++ AULA01/exercicio.js
@@ -0,0 +1,8 @@
+const_qtdPecasPH = 120;
+const horasTurno = 8;
+
+const prodTotal = qntdPecasPH * horasTurno;
+
+console.log(`Producao por hora: ${qntdPecasPH}`)
+console.log(`Horas por turno: ${horasTurno}`)
+console.log(`Producao por turno: ${prodTotal}`)
